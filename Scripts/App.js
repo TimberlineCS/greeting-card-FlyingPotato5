840b9e89eb4ebd92ea957c76d944cdae15c3ce17
FlyingPotato5 <149399929+FlyingPotato5@users.noreply.github.com>
2023-12-11 10:03:00 -0700

    SANS

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,10 +1,21 @@
-//Your name
-//Period
-//Date
+//Connor Wing
+//1
+//12/11/23
 
-/*Provide a description about what this program does here*/
+/*This is a javascript christmas card*/
 
 /*Write code to create and manipulate the elements on the index.html page.  
 The elements must come to gether to resemble an image.  Your Greeting Card
 must also include text, an image, and music. */
 
+//This isn't a html class, it's actually a css class disguised as a javascript class.
+
+var text = document.createElement("div");
+text.id = "coolfont";
+text.innerHTML = "Have a Snowy Christmas!";
+text.style.backgroundColor = "black"
+text.style.color = "white";
+text.style.fontSize = "3em";
+text.style.textAlign = "center";
+
+document.body.append(text);
